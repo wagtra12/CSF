b0cb640315979d669d66f8b31bb01c9665be875b
wagtra12 <twags32@gmail.com>
2013-10-17 13:53:56 -0700

    lab3

---
+++ lab3.py
@@ -0,0 +1,39 @@
+n=13
+series = 'fibonacci'
+
+#if the string series is fibonacci
+if series == "fibonacci":
+    a=1
+    b=0
+   
+    #calculates fibonacci
+    for i in range(n):
+        c=a+b
+        print c
+        a=b
+        b=c
+
+#if the string(series) is sum
+elif series== "sum":
+    number=0
+    sums=0
+    
+    #finds the sum of the multiples of 3 to the nth numbber
+    for i in range(n+1):
+        number= 3 * i
+        sums= number + sums
+    print sums
+    
+#if the string(series) isn;t fib or sum
+else:
+    print "Invalid string"
+    
+#collaborator Brandon
+    
+
+        
+
+
+
+    
+    
