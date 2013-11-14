a82bc0aa6a482ec9de6707818270045d4c11b392
wagtra12 <twags32@gmail.com>
2013-11-14 12:23:11 -0800

    hw6.py

---
+++ hw6.py
@@ -0,0 +1,65 @@
+# Name: Cardin Nguyen, Travis Wagner
+# Evergreen Login: ngutro25, wagtra12
+# Computer Science Foundations
+# Programming as a Way of Life
+# Homework 6
+
+# You may do your work by editing this file, or by typing code at the
+# command line and copying it into the appropriate part of this file when
+# you are done.  When you are done, running this file should compute and
+# print the answers to all the problems.
+
+
+###
+### Problem 3
+###
+
+# DO NOT CHANGE THE FOLLOWING LINE
+print "Problem 3 solution follows:"
+
+# ... write your code and comments here (and remove this line)
+
+
+###
+### Problem 4
+###
+
+# DO NOT CHANGE THE FOLLOWING LINE
+print "Problem 4 solution follows:"
+
+# ... write your code and comments here (and remove this line)
+
+
+###
+### Problem 5
+###
+
+# DO NOT CHANGE THE FOLLOWING LINE
+print "Problem 5 solution follows:"
+
+# ... write your code and comments here (and remove this line)
+
+###
+### Problem 6
+###
+
+# DO NOT CHANGE THE FOLLOWING LINE
+print "Problem 6 solution follows:"
+
+# ... write your code and comments here (and remove this line)
+
+###
+### Problem 7
+###
+
+# DO NOT CHANGE THE FOLLOWING LINE
+print "Problem 7 solution follows:"
+
+# ... write your code and comments here (and remove this line)
+
+###
+### Collaboration
+###
+
+# ... List your collaborators and other sources of help here (websites, books, etc.),
+# ... as a comment (on a line starting with "#").
